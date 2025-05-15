44c5c9a8e3864501d27fdcc0d19158eaf5123b80
Shashank-kaushal <devshashankkaushal@gmail.com>
2025-05-16 00:50:05 +0530

    complete url shortner task.

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const dns = require('dns');
+const { URL } = require('url');
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+// Basic Configuration
+const port = process.env.PORT || 3000;
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(cors());
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.use(bodyParser.urlencoded({extended:true}));
+var urldata = {};
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url;
+  const normalized = /^https?:\/\//i.test(url) ? url : `https://${url}`;
+  const urlObj = new URL(normalized);
+  dns.lookup(urlObj.hostname, (err, addr, fml) => {
+    if(err){
+      res.json({"error":"Invalid Url"});
+    }else{
+      const shorterUrl = Math.floor(Math.random()*10);
+      urldata = {"original_url":normalized, "short_url":shorterUrl}
+      res.json(urldata);
+    }
+  })
+});
+
+app.get('/api/shorturl/:id', (req, res) => {
+  return res.redirect(urldata.original_url);
+})
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/whoami', (req, res) => {
-  res.json({"ipaddress" : req.socket.remoteAddress, "language" : req.headers['accept-language'], "software" : req.headers['user-agent']});
-});
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
